3adaa2267eed0bbc97d5a05ab496ebb3815a8214
NatiCohen10 <nati1236nati@gmail.com>
2024-06-24 22:46:27 +0300

    a

---
+++ src/components/FilterProducts.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+
+function FilterProducts(props) {
+  const { name, setSearchParams, searchParams, minPrice, maxPrice, isInStock } =
+    props;
+
+  const toggleInStock = () => {
+    setSearchParams((prev) => {
+      const isInStock = prev.get("isInStock") === "true";
+      prev.set("isInStock", isInStock ? "false" : "true");
+      return prev;
+    });
+  };
+  return (
+    <div>
+      <div>
+        <label htmlFor="productName">product name</label>
+        <input
+          id="productName"
+          className=" border-2 border-black"
+          type="text"
+          value={name || ""}
+          onChange={(e) =>
+            setSearchParams((prev) => {
+              prev.set("name", e.target.value);
+              return prev;
+            })
+          }
+        />
+      </div>
+
+      <div className="">
+        <label htmlFor="minPrice">minimum price</label>
+        <input
+          className=" border-2 border-black "
+          type="number"
+          id="minPrice"
+          value={minPrice}
+          onChange={(e) =>
+            setSearchParams((prev) => {
+              prev.set("minPrice", e.target.value);
+              return prev;
+            })
+          }
+        />
+      </div>
+      <div className="">
+        <label htmlFor="maxNum">maximum price</label>
+        <input
+          className=" border-2 border-black "
+          type="number"
+          id="maxNum"
+          value={maxPrice}
+          onChange={(e) =>
+            setSearchParams((prev) => {
+              prev.set("maxPrice", e.target.value);
+              return prev;
+            })
+          }
+        />
+      </div>
+      <div className="">
+        <label>
+          <input
+            className="border-2 border-black"
+            type="checkbox"
+            id="available"
+            checked={searchParams.get("isInStock") === "true"}
+            onChange={toggleInStock}
+          />
+          In Stock Only
+        </label>
+      </div>
+    </div>
+  );
+}
+
+export default FilterProducts;
--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -8,12 +8,18 @@ import {
   useSearchParams,
 } from "react-router-dom";
 import ProductItem from "./ProductItem";
+import FilterProducts from "./FilterProducts";
 
 const PRODUCTS_URL = "http://localhost:3000/api/products";
 
 function ProductsList() {
   const [products, setProducts] = useState([]);
-  const [filtered, setFiltered] = useState([]);
+
+  const [pagination, setPagination] = useState({
+    totalItems: 0,
+    totalPages: 1,
+    currentPage: 1,
+  });
   const [searchParams, setSearchParams] = useSearchParams({
     name: "",
     minPrice: "",
@@ -30,7 +36,14 @@ function ProductsList() {
     async function fetchProducts() {
       try {
         const response = await axios.get(PRODUCTS_URL + location.search);
-        setProducts(response.data);
+        const productCount = await axios.get(
+          PRODUCTS_URL + "/count" + location.search
+        );
+        const { limit, products, currentPage } = response.data;
+        const { count } = productCount.data;
+        const totalPages = Math.ceil(count / limit);
+        setProducts(products);
+        setPagination({ count, totalPages, currentPage });
       } catch (error) {
         console.error(error);
       }
@@ -38,22 +51,44 @@ function ProductsList() {
     fetchProducts();
   }, [products, location.pathname]);
 
+  function handlePageChange(newPage) {
+    setSearchParams((prev) => {
+      prev.set("page", newPage);
+      return prev;
+    });
+  }
+
   return (
     <>
       <h2 className=" text-4xl font-bold">All products</h2>
-      <input
-        type="text"
-        value={name || ""}
-        onChange={(e) =>
-          setSearchParams((prev) => {
-            prev.set("name", e.target.value);
-            return prev;
-          })
-        }
+      <FilterProducts
+        name={name}
+        setSearchParams={setSearchParams}
+        searchParams={searchParams}
+        minPrice={minPrice}
+        maxPrice={maxPrice}
+        isInStock={isInStock}
       />
       {products.map((product) => {
         return <ProductItem product={product} key={product._id} />;
       })}
+      <div>
+        <button
+          disabled={pagination.currentPage === 1}
+          onClick={() => handlePageChange(pagination.currentPage - 1)}
+        >
+          Previous
+        </button>
+        <span>
+          Page {pagination.currentPage} of {pagination.totalPages}
+        </span>
+        <button
+          disabled={pagination.currentPage === pagination.totalPages}
+          onClick={() => handlePageChange(pagination.currentPage + 1)}
+        >
+          Next
+        </button>
+      </div>
     </>
   );
 }
